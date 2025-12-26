940f2f77ca85addc63c5fd23171792c3a76c6af6
Sk1lizz <sk1lizz@mail.ru>
2025-12-26 22:00:32 +0300

    26.12.2025

--- functions.py
+++ functions.py
@@ -7,6 +7,7 @@ def switch():
     pass
 
 def info():
+
     # Вводим переменные
     name = cfg.NAME
     version = cfg.VERSION
@@ -17,15 +18,18 @@ def info():
     last_update = cfg.LAST_DATE_UPDATE
     console = cfg.CONSOLE_SUPPORTIVE
     gui = cfg.GUI_SUPPORTIVE
+
     # Текст
     text = f"""--------{name}--------\n     Name: {name}\n     Versions: {version}\n     Creator: {creator}\n     Root: {root}\n     Git-connect: {git_connect}\n     Date create: {date_create}\n     Last date update: {last_update}\n Console: {console}      |      Gui: {gui}\n-------------<INFO  MENU>-------------"""
 
     return text
 
 def help():
+
     # Вводим переменные
     name = cfg.NAME
-
+    
+    # Текст
     text = f"""--------{name}--------\n <1> - Main menu\n <2> - Switch menu\n <8> - Info menu\n <9> - Help menu  \n-------------<HELP  MENU>-------------"""
 
     return text
--- switch.py
+++ switch.py
@@ -10,8 +10,42 @@ def get_path(*args, **kwargs):
 
     return path
 
-def switch_on():
-    pass
+def filter_website(site: str): 
+
+    site_end = (site.replace("https://", "")).replace("http://", "")
+
+    if site_end[0] == "w" and site_end[1] == "w" and site_end[2] == "w" and site_end[3] == ".":
+        site_end=site_end.replace("www.", "")
+
+    return site_end
+
+    
+
+
+def switch_add(site_start):
+    ip_block = "0.0.0.0"
+    path_to_local_hosts_file = r"files\datafile\Windows\hosts\hosts"
+
+    site = filter_website(site_start)
+
+    text = [
+        f"\n\n# Blocking access to the website: {site} (by skilizz)",
+        f"{ip_block} {site}"
+    ]
+
+    try:
+        with open(path_to_local_hosts_file, "a", encoding='utf-8') as file:
+            file.write("\n".join(text))
+            return f"Website {site} added"
+        
+    except Exception as e:
+        return f"Error: {e}"
+
+    
+def switch_on(site):
+    path = get_path()
+    if path == None: return "This platform is not supported."
+
 
 def switch_off():
     pass
